b636bb0e7385949a04a09d34e8cc10cf58ffb6c7
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-09-15 00:00:08 -0600

    diseño minimo

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
-   
+
     <div className="fixed bottom-0 w-full h-16 p-3 bg-gray-800/40 backdrop-blur-sm flex items-center text-gray-100">
-      
+
       <div>
         <h2 className=''>ResourceWeb</h2>
       </div>
       <ul className='flex ml-auto'>
-        <li className='m-2'><FaGithub className='bg-black rounded-full h-6 w-6' /></li>
-        <li className='m-2'><FaGithub className='bg-blue-500 rounded-full h-6 w-6'/></li>
+        <li className='m-2'><FaGithub className='bg-black rounded-full h-8 w-8 hover:bg-black/300 transition-colors duration-300 ease-in-out ring-1 shadow-md' /></li>
+        <li className='m-2'><FaGithub className='bg-blue-500 rounded-full h-8 w-8 hover:bg-blue-300/80 transition-colors duration-300 ease-in-out ring-1 shadow-md' /></li>
       </ul>
       <div className='ml-auto'>
         <h2 className=''>09/14/2025 Proyecto React con Vite</h2>
