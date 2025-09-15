67edd0278d6e315a61aec3cd7bf73944b9c161a0
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-09-14 23:37:38 -0600

    Agregado de diseño en el footer con la importación de react-icons

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,15 +1,17 @@
-// En tu archivo Footer.jsx
 import React from 'react';
-
+import { FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
-    // CAMBIOS AQUÍ:
-    // 1. Usamos bg-gray-800/80, que es la sintaxis más moderna para la opacidad.
-    // 2. Añadimos "backdrop-blur-lg" para crear el efecto de desenfoque.
+   
     <div className="fixed bottom-0 w-full h-16 p-3 bg-gray-800/40 backdrop-blur-sm flex items-center text-gray-100">
       
-      <h2 className=''>ResourceWeb</h2>
-      
+      <div>
+        <h2 className=''>ResourceWeb</h2>
+      </div>
+      <ul className='flex ml-auto'>
+        <li className='m-2'><FaGithub className='bg-black rounded-full h-6 w-6' /></li>
+        <li className='m-2'><FaGithub className='bg-blue-500 rounded-full h-6 w-6'/></li>
+      </ul>
       <div className='ml-auto'>
         <h2 className=''>09/14/2025 Proyecto React con Vite</h2>
       </div>
